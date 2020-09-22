70f96d6e12c89c6f091ea11ba234f535994658d0
t00199360 <t00199360@gmail.com>
2020-09-22 14:50:16 +0100

    Caught up to Robs scene
    Ball bounces somewhat convincingly

---
+++ Bounce/Assets/Physics_Script.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics_Script : MonoBehaviour
+{
+    Vector3 velocity = new Vector3(0, 0, 0);
+    Vector3 acceleration = new Vector3(0, -9.8f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+        if ((transform.position.y - 0.5f) < 0f)
+        {
+            transform.position -= velocity * Time.deltaTime;
+            velocity = -velocity * 0.5f;
+
+        }
+    }
+}
