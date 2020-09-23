aedbaf677a7fbd86f69a5c387b7fad567f9f6ef6
t00199360 <t00199360@gmail.com>
2020-09-23 18:05:38 +0100

    progressed the gravity logic
    up to date with robs scene 23/9/2020

--- Bounce/Assets/Physics_Script.cs
+++ Bounce/Assets/Physics_Script.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Physics_Script : MonoBehaviour
 {
+    PlaneScript plane;
     Vector3 velocity = new Vector3(0, 0, 0);
     Vector3 acceleration = new Vector3(0, -9.8f, 0);
+
+   
 
     // Start is called before the first frame update
     void Start()
     {
-
+        plane = FindObjectOfType<PlaneScript>();
     }
 
     // Update is called once per frame
@@ -19,11 +23,23 @@ public class Physics_Script : MonoBehaviour
         velocity += acceleration * Time.deltaTime;
         transform.position += velocity * Time.deltaTime;
 
-        if ((transform.position.y - 0.5f) < 0f)
+        if(plane.distance_to(transform.position)<= 0.5f)
         {
             transform.position -= velocity * Time.deltaTime;
-            velocity = -velocity * 0.5f;
+            Vector3 parallel = parallel_component(velocity, plane.normal);
+            Vector3 perp = perpendicular_component(velocity, plane.normal);
 
+            velocity = perp - 0.6f * parallel;
         }
+    }
+
+    private Vector3 perpendicular_component(Vector3 vec, Vector3 normal)
+    {
+        return vec - parallel_component(vec, normal);
+    }
+
+    private Vector3 parallel_component(Vector3 vec, Vector3 normal)
+    {
+        return Vector3.Dot(vec, normal) * normal;
     }
 }
